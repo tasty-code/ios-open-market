baebf4560cccd923a1291a651c8a9c354b3558c0
dddowon <dowon4095@naver.com>
2022-10-25 15:44:45 +0900

    feat: Product, Pages 모델 구현

--- OpenMarket/OpenMarket/Model/Product.swift
+++ OpenMarket/OpenMarket/Model/Product.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+struct Product: Decodable {
+    let pageNo: Int
+    let itemsPerPage: Int
+    let totalCount: Int
+    let offset: Int
+    let limit: Int
+    let pages: Pages
+    
+    enum CodingKeys: String, CodingKey {
+        case offset, limit
+        case pageNo = "page_no"
+        case itemPerPage = "item_per_page"
+        case totalCount = "total_count"
+        case pages
+    }
+}
+
+struct Pages: Decodable {
+    let id: Int
+    let vendorId: Int
+    let name: String
+    let thumbnail: String
+    let corrency: String
+    let price: Int
+    let bargainPrice: Int
+    let discountedPrice: Int
+    let stock: Int
+    let createdAt: String
+    let issuedAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, thumbnail, corrency, price, stock
+        case vendorId = "vendor_id"
+        case bargainPrice = "bargain_price"
+        case discountedPrice = "discounted_price"
+        case createdAt = "created_at"
+        case issudeAt = "issued_at"
+    }
+}
