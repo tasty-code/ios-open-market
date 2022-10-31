4a02540067b1828cbc85e985dd23ca59a1cb4130
dddowon <dowon4095@naver.com>
2022-10-31 11:42:49 +0900

    refactor: viewDidLoad에 테스트 함수 제거

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -5,22 +5,12 @@
 // 
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
-    func jsonParsingData() -> Product? {
-        guard let dataAsset = NSDataAsset(name: "products") else {
-            return nil
-        }
-        let jsonDecoder = JSONDecoder()
-        let product = try? jsonDecoder.decode(Product.self, from: dataAsset.data)
-        
-        return product
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let product = jsonParsingData()
-        print(product?.pageNo)
+        
     }
 }
