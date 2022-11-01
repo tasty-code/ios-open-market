ee71ca5c36f5d3aa5da1330f97b1da0939d1971f
dddowon <dowon4095@naver.com>
2022-11-01 16:09:53 +0900

    feat: getOpenMarketData 메서드 구현 - URLSession

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -8,9 +8,11 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController {
+    let network = NetWork()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        network.getOpenMarketData(url: "https://openmarket.yagom-academy.kr/api/products?page_no=1&items_per_page=100")
         
     }
 }
--- OpenMarket/OpenMarket/Model/NetWork.swift
+++ OpenMarket/OpenMarket/Model/NetWork.swift
@@ -16,4 +16,34 @@ struct NetWork {
         case parameter = "?page_no=1&items_per_page=100"
         case productsPathId = "/api/products/32"
     }
+    
+    func getOpenMarketData(url: String, completion: @escaping (Product) -> Void ) {
+        guard let url = URL(string: url) else {
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                print("error")
+                return
+            }
+            
+            if let data = data {
+                do {
+                    let receivedData = try JSONDecoder().decode(Product.self, from: data)
+                    completion(receivedData)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        task.resume()
+    }
 }
