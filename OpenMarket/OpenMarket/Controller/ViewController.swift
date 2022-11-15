80ea2a54699dfd88f4605e1e58adfbd468ed5588
eunho8713 <dmsgh3303@gmail.com>
2022-11-15 15:18:30 +0900

    feat: CollectionView 생성

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -8,9 +8,34 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController {
+    var productData: [Product] = []
+    let netWork = NetWork()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    func getData() {
+        netWork.getProductData { (result: Result<Product, Error> ) in
+            switch result {
+            case .success(let product):
+                self.productData.append(product)
+            case .failure(let failure):
+                print(failure)
+            }
+        }
+    }
 }
+
+//extension ViewController: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        <#code#>
+//    }
+//
+//
+//}
